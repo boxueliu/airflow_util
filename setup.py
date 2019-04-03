590b46767beb9750196e8db52076443266b3551d
liuboxue <liu.boxue@detvista.com>
2019-04-03 14:12:11 +0800

    upload code

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 setup(
     name="airflowUtil",
-    version="0.2.0",
+    version="0.3.0",
     author="boxue liu",
     author_email="liu.boxue@detvista.com",
     license="Apache License",
